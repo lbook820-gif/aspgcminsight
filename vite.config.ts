995397e81147821f3a7bb4a645100cbd338d5c81
lbook820-gif <lbook820@gmail.com>
2026-04-21 16:43:25 +0100

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/aspg-insight/',
+  base: '/aspgcminsight/',
   plugins: [react()],
   resolve: {
     alias: {
